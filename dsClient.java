a0b2a40bd612ac5cb5c238e00719012721e13ec8
mingxinyang <mingxin.yang@students.mq.edu.au>
2023-04-03 00:24:18 +1000

    Fixed the variables of StageOne

--- dsClient.java
+++ dsClient.java
@@ -1,48 +1,79 @@
 import java.io.*;
 import java.net.*;
 
-
 public class dsClient {
     public static void main(String[] args) {
-         Socket s = new Socket("127.0.0.1", 50000);
-        BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        PrintWriter dos = new PrintWriter(s.getOutputStream(), true);
-        
-        dos.println("HELO");
-        dis.readLine();
-        dos.println("AUTH " + System.getProperty("user.name"));
-        dis.readLine();
-        dos.println("REDY");
+        try {
+            //initialize job related data variables below
+            int maxAvail = 0;
+            int numServers = 0;
+            String serverType = "";
+            int jobID = 0;
+            int n = 0;
 
-        String inputLine;
-        while ((inputLine = dis.readLine()) != null) {
-            System.out.println("Input line: " + inputLine);
-            if (inputLine.startsWith("JOBN")) {
-                String[] jobDetails = inputLine.split(" ");
-                int jobId = Integer.parseInt(jobDetails[2]);
-                int jobCores = Integer.parseInt(jobDetails[4]);
-                int jobMemory = Integer.parseInt(jobDetails[5]);
-                int jobDisk = Integer.parseInt(jobDetails[6]);
+            //create a socket at localhost of port 50000
+            Socket s = new Socket("127.0.0.1", 50000);
 
-                   
+         DataOutputStream dataOutput = new DataOutputStream(s.getOutputStream());
+         BufferedReader dataInput =new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-                    for (int i = 0; i < numServers; i++) {
-                    if(str!=null){
-                        str = dis.readLine();
-                        String[] serverInfo = str.split("\\s+");
-                        int serverAvail = Integer.parseInt(serverInfo[4]);
-                        if (serverAvail > maxAvail) {
-                            serverType = serverInfo[0];
-                            maxAvail = serverAvail;
+            dataOutput.write(("HELO" + "\n").getBytes());
+            String str = dataInput.readLine();
+            System.out.println("message= " + str);
+
+            //authenticate using my Ubuntu username
+            String username="mingxinyang";
+            dataOutput.write(("AUTH " + username+"\n").getBytes());
+            str = dataInput.readLine();
+            System.out.println("Server: " + str);
+
+            //execute below codes when system does not send NONE
+            while(!str.contains("NONE")){
+                // send REDY to ds-server
+                dataOutput.write(("REDY\n").getBytes());
+                str = dataInput.readLine();
+                System.out.println("Server: " + str);
+                String lastMsg = str;
+
+                // if the message is JOBN, get the job ID
+                if(str.contains("JOBN")){
+                    String[] arr = str.split(" ");
+                    jobID = Integer.parseInt(arr[2]);
+                }
+
+                //obtain the maxAvail core and the NO. of it
+                if(maxAvail == 0){
+                    dataOutput.write(("GETS All\n").getBytes());
+                    str = dataInput.readLine();
+                    System.out.println("Server: " + str);
+                    String[] data = str.split(" ");
+                    int numRecords = Integer.parseInt(data[1]);
+                    dataOutput.write(("OK\n").getBytes());
+
+                    for(int i=0; i<numRecords; i++){
+                        str = dataInput.readLine();
+                        System.out.println("Server: " + str);
+
+                        String[] arr = str.split(" ");
+                        int coreCount = Integer.parseInt(arr[4]);
+
+                        if(coreCount > maxAvail){
+                           maxAvail = coreCount;
+                            serverType = arr[0];
+                            numServers = 0;
+                        } else if(coreCount == maxAvail && serverType.equals(arr[0])){
+                            numServers++;
                         }
                     }
-                    }
-                    dos.write(("OK\n").getBytes());
-                    str = dis.readLine();
 
-                    //schedule it when the msg ==JOBN
+                    dataOutput.write(("OK\n").getBytes());
+                    str = (String)dataInput.readLine();
+                    System.out.println("Server: " + str);
+                }
+
+                //schedule it when the msg ==JOBN
                 if(lastMsg.contains("JOBN")){
-                    dataOutput.write(("SCHD " +jobId+ " "+serverType+" "+n +"\n").getBytes());
+                    dataOutput.write(("SCHD " +jobID+ " "+serverType+" "+n +"\n").getBytes());
                     str = dataInput.readLine();
                     System.out.println("Server: " +str);
                     n++;
