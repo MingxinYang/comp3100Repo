eea192a90042e890e8fff8e3c895aba72aa38dfd
MingxinYang <87561186+MingxinYang@users.noreply.github.com>
2023-04-03 14:55:22 +1000

    fixed minors

--- dsClient.java
+++ dsClient.java
@@ -4,98 +4,88 @@ import java.net.*;
 public class dsClient {
     public static void main(String[] args) {
         try {
-            //initialize job related data variables below
-            int maxAvail = 0;
-            int numServers = 0;
-            String serverType = "";
-            int jobID = 0;
-            int n = 0;
+            int maxCores = 0;
+            int totalServers = 0;
+            String sType = "";
+            int jobId = 0;
+            int currentIndex = 0;
 
-            //create a socket at localhost of port 50000
-            Socket s = new Socket("127.0.0.1", 50000);
+            Socket socket = new Socket("localhost", 50000);
 
-         DataOutputStream dataOutput = new DataOutputStream(s.getOutputStream());
-         BufferedReader dataInput =new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+            BufferedReader dataIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            dataOutput.write(("HELO" + "\n").getBytes());
-            String str = dataInput.readLine();
-            System.out.println("message= " + str);
+            dataOut.write(("HELO" + "\n").getBytes());
+            String response = dataIn.readLine();
+            System.out.println("Received: " + response);
 
-            //authenticate using my Ubuntu username
-            String username="mingxinyang";
-            dataOutput.write(("AUTH " + username+"\n").getBytes());
-            str = dataInput.readLine();
-            System.out.println("Server: " + str);
+            String user = "mingxinyang";
+            dataOut.write(("AUTH " + user + "\n").getBytes());
+            response = dataIn.readLine();
+            System.out.println("Server: " + response);
 
-            //execute below codes when system does not send NONE
-            while(!str.contains("NONE")){
-                // send REDY to ds-server
-                dataOutput.write(("REDY\n").getBytes());
-                str = dataInput.readLine();
-                System.out.println("Server: " + str);
-                String lastMsg = str;
+            while (!response.contains("NONE")) {
+                dataOut.write(("REDY\n").getBytes());
+                response = dataIn.readLine();
+                System.out.println("Server: " + response);
+                String previousMsg = response;
 
-                // if the message is JOBN, get the job ID
-                if(str.contains("JOBN")){
-                    String[] arr = str.split(" ");
-                    jobID = Integer.parseInt(arr[2]);
+                if (response.contains("JOBN")) {
+                    String[] arr = response.split(" ");
+                    jobId = Integer.parseInt(arr[2]);
                 }
 
-                //obtain the maxAvail core and the NO. of it
-                if(maxAvail == 0){
-                    dataOutput.write(("GETS All\n").getBytes());
-                    str = dataInput.readLine();
-                    System.out.println("Server: " + str);
-                    String[] data = str.split(" ");
+                if (maxCores == 0) {
+                    dataOut.write(("GETS All\n").getBytes());
+                    response = dataIn.readLine();
+                    System.out.println("Server: " + response);
+                    String[] data = response.split(" ");
                     int numRecords = Integer.parseInt(data[1]);
-                    dataOutput.write(("OK\n").getBytes());
+                    dataOut.write(("OK\n").getBytes());
 
-                    for(int i=0; i<numRecords; i++){
-                        str = dataInput.readLine();
-                        System.out.println("Server: " + str);
+                    for (int i = 0; i < numRecords; i++) {
+                        response = dataIn.readLine();
+                        System.out.println("Server: " + response);
 
-                        String[] arr = str.split(" ");
+                        String[] arr = response.split(" ");
                         int coreCount = Integer.parseInt(arr[4]);
 
-                        if(coreCount > maxAvail){
-                           maxAvail = coreCount;
-                            serverType = arr[0];
-                            numServers = 0;
-                        } else if(coreCount == maxAvail && serverType.equals(arr[0])){
-                            numServers++;
+                        if (coreCount > maxCores) {
+                            maxCores = coreCount;
+                            sType = arr[0];
+                            totalServers = 0;
+                        } else if (coreCount == maxCores && sType.equals(arr[0])) {
+                            totalServers++;
                         }
                     }
 
-                    dataOutput.write(("OK\n").getBytes());
-                    str = (String)dataInput.readLine();
-                    System.out.println("Server: " + str);
+                    dataOut.write(("OK\n").getBytes());
+                    response = dataIn.readLine();
+                    System.out.println("Server: " + response);
                 }
 
-                //schedule it when the msg ==JOBN
-                if(lastMsg.contains("JOBN")){
-                    dataOutput.write(("SCHD " +jobID+ " "+serverType+" "+n +"\n").getBytes());
-                    str = dataInput.readLine();
-                    System.out.println("Server: " +str);
-                    n++;
+                if (previousMsg.contains("JOBN")) {
+                    dataOut.write(("SCHD " + jobId + " " + sType + " " + currentIndex + "\n").getBytes());
+                    response = dataIn.readLine();
+                    System.out.println("Server: " + response);
+                    currentIndex++;
 
-                    if(n >= numServers){//when n higher than the NO. servers
-                        n = 0;
+                    if (currentIndex >= totalServers) {
+                        currentIndex = 0;
                     }
                 }
             }
 
-            // ds-server sends QUIT and receive QUIT
-            dataOutput.write(("QUIT\n").getBytes());
-            str = dataInput.readLine();
-            System.out.println("Server: " + str);
+            dataOut.write(("QUIT\n").getBytes());
+            response = dataIn.readLine();
+            System.out.println("Server: " + response);
 
-            // close the socket, I/O stream
-            dataInput.close();
-            dataOutput.close();
-            s.close();
+            dataIn.close();
+            dataOut.close();
+            socket.close();
 
         } catch (Exception e) {
             System.out.println(e);
         }
-   }
+    }
 }
