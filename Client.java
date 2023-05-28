5a6e211f874481ee26f76600fd9d4a0cfea2100d
MingxinYang <87561186+MingxinYang@users.noreply.github.com>
2023-05-28 17:01:55 +1000

    server side for stage 2

--- Client.java
+++ Client.java
@@ -1,36 +1,19 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 import java.util.stream.Collectors;
-import java.util.HashMap;
-import java.util.Map;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
-import javax.xml.parsers.DocumentBuilderFactory;
 
- class Job {
-
- 
+class Job {
     String basicInfo = "";
-//task sub info
     int subTime;
     int IDs;
     int RTime;
     int coreNum;
-    int memSize
+    int memSize;
     int disk;
 
-   // Initialize the properties of job-data
     public static Job parseSimpleJob(String msg) {
         String[] info = msg.trim().split("\\s+");
         Job job = new Job();
@@ -38,7 +21,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
             job.basicInfo = info[0];
             job.subTime = Integer.parseInt(info[1]);
             job.IDs = Integer.parseInt(info[2]);
-            job.RTime= Integer.parseInt(info[3]);
+            job.RTime = Integer.parseInt(info[3]);
             job.coreNum = Integer.parseInt(info[4]);
             job.memSize = Integer.parseInt(info[5]);
             job.disk = Integer.parseInt(info[6]);
@@ -48,7 +31,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
         return job;
     }
 
-
     public boolean isNone() {
         return basicInfo.equals(Command.NONE.getName());
     }
@@ -57,16 +39,134 @@ import javax.xml.parsers.DocumentBuilderFactory;
         return basicInfo.equals(Command.JCPL.getName());
     }
 
-
     public String toString() {
         return "Job{" +
-            "basicInfo='" + basicInfo + '\'' +
-            ", subTime=" + subTime +
-            ", IDs=" + IDs +
-            ", RTime=" + RTime +
-            ", coreNum=" + coreNum +
-            ", memSize=" + memSize +
-            ", disk=" + disk +
-            '}';
+                "basicInfo='" + basicInfo + '\'' +
+                ", subTime=" + subTime +
+                ", IDs=" + IDs +
+                ", RTime=" + RTime +
+                ", coreNum=" + coreNum +
+                ", memSize=" + memSize +
+                ", disk=" + disk +
+                '}';
+    }
+}
+
+enum JobState {
+    Submitted(0), Waiting(1), Running(2), Suspended(3), Completed(4), PreEmpted(5), Failed(6), Killed(7);
+
+    JobState(int i) {
+    }
+
+    public static JobState parse(int type) {
+        if (type < 0 || type >= values().length) {
+            return null;
+        }
+        return values()[type];
+    }
+}
+
+class Server {
+    String type;
+    int IDs;
+    ServerState state;
+    int curStartTime;
+    int coreNum;
+    int memSize;
+    int disk;
+    int wJobs;
+    int rJobs;
+    int bootTime;
+    int estTime;
+    float hourlyRate;
+    static Map<String, Integer> bootTimeMap = null;
+    static Map<String, Float> hourlyRateMap = null;
+    static final int SIMPLE_SERVER_FIELDS = 9;
+
+    private Server() {
+        if (bootTimeMap == null) {
+            buildBootTime();
+        }
+    }
+
+    public static Server parseSimpleServer(String msg) {
+        String[] info = msg.trim().split("\\s+");
+        Server server = new Server();
+        if (info.length < SIMPLE_SERVER_FIELDS) {
+            throw new RuntimeException("Insufficient number of server properties");
+        }
+        try {
+            server.type = info[0];
+            server.IDs = Integer.parseInt(info[1]);
+            server.state = ServerState.parse(info[2]);
+            server.curStartTime = Integer.parseInt(info[3]);
+            server.coreNum = Integer.parseInt(info[4]);
+            server.memSize = Integer.parseInt(info[5]);
+            server.disk = Integer.parseInt(info[6]);
+            server.wJobs = Integer.parseInt(info[7]);
+            server.rJobs = Integer.parseInt(info[8]);
+            server.bootTime = bootTimeMap.get(server.type);
+            server.hourlyRate = hourlyRateMap.get(server.type);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return server;
+    }
+
+    public boolean meetJobLevel(Job job) {
+        return memSize >= job.memSize && coreNum >= job.coreNum && disk >= job.disk;
+    }
+
+    public String toString() {
+        return String.format("[%s, %d, %s, %d, %d, %d, %d, %d, %d]",
+                type, IDs, state, curStartTime, coreNum, memSize, disk, wJobs, rJobs);
+    }
+
+    public static void buildBootTime() {
+        bootTimeMap = new HashMap<>();
+        hourlyRateMap = new HashMap<>();
+        NodeList list = null;
+        try {
+            list = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("ds-system.xml").getElementsByTagName("servers");
+            for (int i = 0; i < list.getLength(); i++) {
+                Element element = (Element) list.item(i);
+                NodeList childNodes = element.getChildNodes();
+                for (int j = 0; j < childNodes.getLength(); j++) {
+                    NamedNodeMap attributes = childNodes.item(j).getAttributes();
+                    if (attributes == null) {
+                        continue;
+                    }
+                    String type = attributes.getNamedItem("type").getNodeValue();
+                    int bootTime = Integer.parseInt(attributes.getNamedItem("bootupTime").getNodeValue());
+                    float hourlyRate = Float.parseFloat(attributes.getNamedItem("hourlyRate").getNodeValue());
+                    bootTimeMap.put(type, bootTime);
+                    hourlyRateMap.put(type, hourlyRate);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+}
+
+enum ServerState {
+    Inactive, Booting, Idle, Active, Unavailable;
+
+    static ServerState parse(String state) {
+        switch (state) {
+            case "inactive":
+                return Inactive;
+            case "booting":
+                return Booting;
+            case "idle":
+                return Idle;
+            case "active":
+                return Active;
+            case "unavailable":
+                return Unavailable;
+            default:
+                throw new RuntimeException("Unsupported server state");
+        }
     }
 }
